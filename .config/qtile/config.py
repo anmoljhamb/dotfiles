933e05264dc33b90f1ed86f1564eee4907c89eff
Anmol Jhamb <talktoanmol@outlook.com>
2024-01-27 09:03:00 +0530

    modify the top bar.

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -189,21 +189,11 @@ screens = [
             [
                 widget.CurrentLayout(),
                 widget.GroupBox(),
-                widget.Prompt(),
                 widget.WindowName(),
-                widget.Chord(
-                    chords_colors={
-                        "launch": ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
-                ),
-                widget.TextBox("default config", name="default"),
-                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
-                # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
-                # widget.StatusNotifier(),
                 widget.Systray(),
                 widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
                 widget.QuickExit(),
+                widget.Battery(),
             ],
             24,
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
