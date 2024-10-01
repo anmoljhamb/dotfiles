b392ffae454c5c0e08112b8f4f365eba962326d3
Anmol Jhamb <talktoanmol@outlook.com>
2024-10-01 22:09:24 +0530

    setup_git done

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
+NAME="Anmol Jhamb"
+EMAIL="talktoanmol@outlook.com"
+
 installPreReq(){
-  PRE_REQS=$(tr '\n' ' ' < ./pre-reqs.txt)
-  sudo apt install $PRE_REQS
+  cat << EOF
+---
+1. Installing prerequisites.
+EOF
+
+  pre_reqs=$(tr '\n' ' ' < ./pre-reqs.txt)
+  echo "$pre_reqs" | xargs sudo apt install 
+}
+
+setupGit(){
+  cat << EOF
+---
+2. Setting Up git.
+EOF
+  ssh_file=~/.ssh/heyitsanmolj_github
+  git config --global user.email "$EMAIL"
+  git config --global user.name "$NAME"
+  ssh-keygen -t ed25519 -C "$EMAIL" -f $ssh_file
+  eval "$(ssh-agent -s)"
+  ssh-add $ssh_file
+  xclip -selection clipboard -i "$ssh_file.pub"
 }
 
 installPreReq
+setupGit
