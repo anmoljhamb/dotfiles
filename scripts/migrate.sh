da72adce626825b4272fcd6cb455e34b64cb3e28
Anmol Jhamb <talktoanmol@outlook.com>
2024-10-03 18:27:50 +0530

    setup other stuff

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -23,13 +23,13 @@ notify(){
 ---
 ${1}: ${2}
 EOF
+((counter++))
 }
 
 installPreReq(){
   notify $counter "Intalling PreRequesites"
   pre_reqs=$(tr '\n' ' ' < ./pre-reqs.txt)
   echo "$pre_reqs" | xargs sudo apt install -y 
-  ((counter++))
   pip install -r ~/dotfiles/requirements.txt --break-system-packages
 }
 
@@ -39,7 +39,6 @@ installChrome(){
   wget $chrome_url -O /tmp/google-chrome-stable_current_amd64.deb
   sudo apt install -y /tmp/google-chrome-stable_current_amd64.deb
   rm -f /tmp/google-chrome-stable_current_amd64.deb
-  ((counter++))
 }
 
 setupGit(){
@@ -51,7 +50,6 @@ setupGit(){
   eval "$(ssh-agent -s)"
   ssh-add $ssh_file
   xclip -selection clipboard -i "$ssh_file.pub"
-  ((counter++))
 }
 
 linkAll(){
@@ -59,7 +57,32 @@ linkAll(){
   python3 ~/dotfiles/link_all.py
 }
 
+setupNpm(){
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+  # shellcheck source=/dev/null
+  source ~/.bashrc
+  nvm install --lts
+}
+
+setupNvim(){
+  notify $counter "Setup NeoVim"
+  wget https://github.com/neovim/neovim/releases/download/v0.10.2/nvim.appimage -O ~/Applications/nvim.appimage
+  chmod u+x ~/Applications/nvim.appimage
+}
+
+setupZsh(){
+  notify $counter "Setup ZSH"
+  chsh -s /bin/zsh
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  sudo dpkg-reconfigure locales
+  rm ~/.zshrc
+}
+
 confirm "Install PreRequesites?" installPreReq
 confirm "Install Google Chrome?" installChrome
 confirm "Setup git?" setupGit
+confirm "Setup npm?" setupNpm
+confirm "Setup nvim?" setupNvim
+confirm "Setuo zsh?" setupZsh
 confirm "Link All Dotfiles?" linkAll
