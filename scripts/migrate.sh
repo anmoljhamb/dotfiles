ff6cd221753ed834995018db8c441f23247e2b8e
Anmol Jhamb <talktoanmol@outlook.com>
2024-10-04 15:13:06 +0530

    setupNpm fixed

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -62,8 +62,9 @@ linkAll(){
 
 setupNpm(){
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-  # shellcheck source=/dev/null
-  source ~/.bashrc
+  export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
   nvm install --lts
 }
 
