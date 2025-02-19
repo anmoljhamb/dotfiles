57903c261b0a2113a59f41ea16c12e9e864ddec2
Anmol Jhamb <talktoanmol@outlook.com>
2025-02-19 16:47:15 +0530

    delete all the not necessary files

--- .config/qtile/utils.py
+++
@@ -1,5 +0,0 @@
-def calc_center(h: float):
-    assert h > 0 and h <= 1
-    remaining_space = 1 - h
-    remaining_space = remaining_space / 2
-    return remaining_space
--- scripts/set_wallpaper.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export DISPLAY=:0
-random_wallpaper=$1
-nitrogen --set-zoom-fill "$random_wallpaper"
